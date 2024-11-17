1777e5ae9e553e867d51f6c1b9357eeec723b075
Richmond Ko <richmondchuako@gmail.com>
2024-11-17 21:34:58 +0100

    Update readme

---
+++ RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,16 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Richmond and Loren on 2024-11-17.
+//
+
+import Testing
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
